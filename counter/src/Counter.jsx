1be4e4e0bd35adfed3ff7cf85b77cdbd3a6cb115
bael4 <bayel.ryspekov@gmail.com>
2022-08-11 14:23:13 +0600

    update counter

--- counter/src/Counter.jsx
+++ counter/src/Counter.jsx
@@ -1,11 +1,15 @@
-import React, {useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Counter = () => {
 
+const [timer,setTimer] = useState(0)
 const [counter,setCounter] = useState(0)
 
 
+const [control,setControl] = useState(false)
+
 function increment (){
+    setControl(false)
   setCounter(counter + 1)
 }
 
@@ -14,16 +18,34 @@ function decrement (){
 }
 
 function reset (){
+    setTimer(0)
     setCounter(0)
 }
 
+useEffect(()=>{
+    
+    const autoDec = setInterval(()=>{
+        if(timer && timer< 1000){
+            setControl(true)
+            setTimer(timer-1)
+        }
+        if(!timer){
+            setControl(false)
+        }
+
+        
+    },1000)
+
+    return ()=> clearInterval(autoDec)
+},[timer])
 
   return (
     <div>
-      <h1>{counter}</h1>
-      <button onClick={increment}>increase</button>
-      <button onClick={decrement}>decrease</button>
+      <h1>{counter}-----{timer}</h1>
+      <button disabled={control} onClick={increment}>increase</button>
+      <button disabled={control} onClick={decrement}>decrease</button>
       <button onClick={reset}>reset</button>
+      <input type={'number'} value={control?timer:null} onChange={e=> setTimer(e.target.value)} placeholder='введите'/>
     </div>
   )
 }
