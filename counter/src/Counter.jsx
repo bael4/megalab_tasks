5638cee26d2f2790b6a3b68e61f0656d344b1b0f
bael4 <bayel.ryspekov@gmail.com>
2022-08-14 00:14:29 +0600

    add new update counter

--- counter/src/Counter.jsx
+++ counter/src/Counter.jsx
@@ -1,53 +1,54 @@
-import React, { useEffect, useState } from 'react'
+import React,{useState,useEffect} from "react"
+
+
 
 const Counter = () => {
 
-const [timer,setTimer] = useState(0)
-const [counter,setCounter] = useState(0)
-
-
-const [control,setControl] = useState(false)
-
-function increment (){
-    setControl(false)
-  setCounter(counter + 1)
-}
-
-function decrement (){
-    setCounter(counter - 1)
-}
-
-function reset (){
-    setTimer(0)
-    setCounter(0)
-}
-
-useEffect(()=>{
-    
-    const autoDec = setInterval(()=>{
-        if(timer && timer< 1000){
-            setControl(true)
-            setTimer(timer-1)
-        }
-        if(!timer){
-            setControl(false)
-        }
-
-        
-    },1000)
-
+  const [counter,setCounter] = useState(0)
+  
+  
+  const [isDisableBtns,setIsDisableBtns] = useState(false)
+  
+  function increment (){
+      setCounter(counter + 1)
+  }
+  
+  function decrement (){
+      setCounter(counter - 1)
+  }
+  
+  function reset (){
+      setCounter(0)
+  }
+  
+  useEffect(()=>{
+      let autoDec = null
+      if(!counter){
+        setIsDisableBtns(false)
+      }
+      if(isDisableBtns){
+      autoDec = setInterval(()=>{
+          if(counter && counter< 1000){
+              setCounter(counter-1)
+          }
+      },1000)
+    }
     return ()=> clearInterval(autoDec)
-},[timer])
+  },  [counter])
+  
+    return (
+      <div>
+        <h1>{counter}</h1>
+        <button disabled={isDisableBtns} onClick={increment}>increase</button>
+        <button disabled={isDisableBtns} onClick={decrement}>decrease</button>
+        <button onClick={reset}>reset</button>
+        <input type={'number'} value={counter}
+        onChange={e=> {
+          setCounter(+e.target.value > 1000 ? counter : e.target.value)
+          setIsDisableBtns(true)
+        }} placeholder='ââåäèòå'/>
+      </div>
+    )
+  }
 
-  return (
-    <div>
-      <h1>{counter}-----{timer}</h1>
-      <button disabled={control} onClick={increment}>increase</button>
-      <button disabled={control} onClick={decrement}>decrease</button>
-      <button onClick={reset}>reset</button>
-      <input type={'number'} value={control?timer:null} onChange={e=> setTimer(e.target.value)} placeholder='введите'/>
-    </div>
-  )
-}
-
-export default Counter
+  export default Counter
