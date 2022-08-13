9c4a797c5897533d81d8f9abb5fba9163699216e
bael4 <bayel.ryspekov@gmail.com>
2022-08-14 00:19:57 +0600

    update function

--- index.js
+++ index.js
@@ -2,16 +2,16 @@
 
 
 
-function some(words){
-    let arr = []
-    words.split(' ').forEach(el => {
-        if(el.length>5){
-            arr.push(el.split('').reverse().join(''))
-        } else{
-            arr.push(el)
-        }
-    });
-    return arr.join(' ')
-}
-    let res = some('Hey hello  Javascript')
-    document.getElementById('words').innerHTML = res
+// function some(words){
+//     let arr = []
+//     words.split(' ').forEach(el => {
+//         if(el.length>5){
+//             arr.push(el.split('').reverse().join(''))
+//         } else{
+//             arr.push(el)
+//         }
+//     });
+//     return arr.join(' ')
+// }
+//     let res = some('Hey hello  Javascript')
+//     document.getElementById('words').innerHTML = res
