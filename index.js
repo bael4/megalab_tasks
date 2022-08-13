d90c6f4e3cfcc7e87d3b60f7bcd8a4b546954f85
bael4 <bayel.ryspekov@gmail.com>
2022-08-14 00:28:46 +0600

    fix function updownWord

--- index.js
+++ index.js
@@ -15,3 +15,13 @@
 // }
 //     let res = some('Hey hello  Javascript')
 //     document.getElementById('words').innerHTML = res
+
+function f(str){
+    return str.split(' ').reduce((prev,cur) =>{
+       if(cur.length >=5){ 
+           return `${prev} ${cur.split('').reverse().join('')}`
+       }
+        return `${prev} ${cur}`
+    },'')
+}
+console.log(f('Hey hello  Javascript'))
